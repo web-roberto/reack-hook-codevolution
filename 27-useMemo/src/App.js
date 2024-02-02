83ed5652daa79cd9000484bce73aef04fe592b2c
web-roberto <roberto.alonso.gandia@gmail.com>
2024-02-02 20:07:42 +0100

    final summit

---
+++ 27-useMemo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterOne from '../../32-CustomHook-useCounter/CounterOne';
+import CounterOTwo from '../../32-CustomHook-useCounter/CounterTwo';
+
+function App() {
+  return (
+    <div className="App">
+     <CounterOne />
+     <CounterOTwo />
+    </div>
+  );
+}
+
+export default App;
